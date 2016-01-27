4fd355f55c39f398c403632e4de65e00af0af785
Etienne Lessard <elessard@avencall.com>
2016-01-27 10:05:28 -0500

    update copyright comments

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 by Avencall
+# SPDX-License-Identifier: GPL-3.0+
+
 from fabric.api import run, sudo, env
 
 env.hosts = ['root@xivo-benchmark.lan-quebec.avencall.com']
