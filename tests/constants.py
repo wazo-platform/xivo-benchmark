f44c6c648410d75ddf7b43bcab31fd050925d31a
Francois Blackburn <fblackburn@wazo.community>
2018-11-27 15:56:35 -0500

    update domain name to lan.wazo.io

--- tests/constants.py
+++ tests/constants.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 by Avencall
+# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 import os.path
@@ -7,4 +7,4 @@ import os.path
 ASSET_DIR = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     '..', 'assets')
-HOST = 'xivo-benchmark.lan.proformatique.com'
+HOST = 'xivo-benchmark.lan.wazo.io'
