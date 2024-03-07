8e47dfb8d7ba33a94d0934ddf958e1a8715a4dfa
Sebastien Duthil <sduthil@wazo.community>
2024-03-07 13:22:18 +0100

    fix csv import test
    How:
    
    * Detect context names instead of hardcoded names

--- wazo_benchmark/tests/test_csv_import.py
+++ wazo_benchmark/tests/test_csv_import.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import os.path
@@ -30,8 +30,22 @@ def test_csv_import():
         token=token,
     )
 
+    contexts = client.contexts.list()
+
+    internal_contexts = [context['name'] for context in contexts['items'] if context['type'] == 'internal']
+    if not internal_contexts:
+        raise Exception('Could not find any internal context with username "admin"')
+    internal_context = internal_contexts[0]
+
+    incall_contexts = [context['name'] for context in contexts['items'] if context['type'] == 'incall']
+    if not incall_contexts:
+        raise Exception('Could not find any incall context with username "admin"')
+    incall_context = incall_contexts[0]
+
+    file_path = os.path.join(constants.ASSET_DIR, '100entries.csv')
+    csv_data = read_csv(file_path, internal_context, incall_context)
     start = datetime.now()
-    result = upload_csv(client, token_data['metadata']['tenant_uuid'])
+    result = upload_csv(client, token_data['metadata']['tenant_uuid'], csv_data)
     stop = datetime.now()
 
     assert 'created' in result, f'Result should contains the created users:\n{result}'
@@ -42,8 +56,13 @@ def test_csv_import():
     time.sleep(5)
 
 
-def upload_csv(client, tenant_uuid):
-    filepath = os.path.join(constants.ASSET_DIR, '100entries.csv')
-    with open(filepath, 'rb') as f:
-        csvdata = f.read()
-        return client.users.import_csv(csvdata, tenant_uuid=tenant_uuid)
+def read_csv(file_path, internal_context_name, incall_context_name):
+    with open(file_path, 'rb') as f:
+        csv_data = f.read()
+    csv_data = csv_data.replace(b'INTERNAL_CONTEXT', bytes(internal_context_name, 'utf-8'))
+    csv_data = csv_data.replace(b'INCALL_CONTEXT', bytes(incall_context_name, 'utf-8'))
+    return csv_data
+
+
+def upload_csv(client, tenant_uuid, csv_data):
+    return client.users.import_csv(csv_data, tenant_uuid=tenant_uuid)
