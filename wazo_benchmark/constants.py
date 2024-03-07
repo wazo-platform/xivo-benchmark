76b0ae4a53de42e19a65934f96b81b12f4c195da
Sebastien Duthil <sduthil@wazo.community>
2024-03-07 09:56:11 +0100

    update benchmark hostname

--- wazo_benchmark/constants.py
+++ wazo_benchmark/constants.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import os.path
@@ -6,4 +6,4 @@ import os.path
 ASSET_DIR = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     '..', 'assets')
-HOST = 'xivo-benchmark.lan.wazo.io'
+HOST = 'wazo-benchmark.lan.wazo.io'
